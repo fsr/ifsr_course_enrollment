cd74da7087e6a1eee77d83523af1848045a1fff1
JustusAdam <development@justusadam.com>
2015-03-15 16:09:57 +0100

    fixed original error

--- CManagement/CManagement/urls.py
+++ CManagement/CManagement/urls.py
@@ -7,8 +7,8 @@ import debug_toolbar
 admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^kurse/', include('course_manager.urls', namespace="cmanagement")),
                        url(r'^$', View_Participants.index, name='index'),
                        url(r'^index/$', View_Participants.index, name='index'),
-                       url(r'^_debug_/', include(debug_toolbar.urls))
+                       url(r'^__debug__/', include(debug_toolbar.urls)),
+                       url(r'^', include('course_manager.urls', namespace="cmanagement")),
                        )
